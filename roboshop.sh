6107aa03e1ae2b0757ff83e2068dea8c51863466
pydiraju <pydiraju.tech@gmail.com>
2026-09-02 01:26:18 +0530

    roboshop1

--- roboshop.sh
+++ roboshop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SG_ID="sg-0e979c41f3933b1d9"
+SG_ID="sg-0e979c41f3933b1d9" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
-do 
+do
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
@@ -13,20 +13,20 @@ do
     --query 'Instances[0].InstanceId' \
     --output text )
 
-   if [ $instance == "frontend" ]; then
-      IP=$(
-          aws ec2 describe-instances \  
-          --instance-ids $INSTANCE_ID \
-          --query 'Reservations[].Instances[].PrivateIpAddress' \
-          --output text
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
         )
     else
-        ip=$(
-            aws ec2 describe-instances \  
-          --instance-ids $INSTANCE_ID \
-          --query 'Reservations[].Instances[].PrivateIpAddress' \
-          --output text 
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
         )
-        fi
-        echo "ip address : $IP"
-    done
+    fi
+
+    echo "IP Address: $IP"
